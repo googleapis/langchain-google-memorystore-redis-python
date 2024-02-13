d9a373cd61b0a29467cec98acaa49f3f969f1690
Craig Chi <craigchi@google.com>
2024-02-13 08:17:52 -0800

    feat: add MemorystoreDocumentSaver class

---
+++ src/langchain_google_memorystore_redis/doc_saver.py
@@ -0,0 +1,97 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import uuid
+from typing import Optional, Sequence, Set
+
+import redis
+from langchain_core.documents.base import Document
+
+
+class MemorystoreDocumentSaver:
+    """Document Saver for Cloud Memorystore for Redis database."""
+
+    def __init__(
+        self,
+        client: redis.Redis,
+        content_field: str,
+        metadata_fields: Optional[Set[str]] = None,
+        key_prefix: Optional[str] = None,
+    ):
+        """Initializes the Document Saver for Memorystore for Redis.
+
+        Args:
+            client: A redis.Redis client object.
+            content_field: The field of the hash that Redis uses to store the
+                page_content of the Document.
+            metadata_fields: The metadata fields of the Document that will be
+                stored in the Redis. If None, Redis stores all metadata fields.
+            key_prefix: A prefix for the key used to store history for this
+                session.
+        """
+
+        self._redis = client
+        self._content_field = content_field
+        self._metadata_fields = metadata_fields
+        self._key_prefix = key_prefix if key_prefix else ""
+
+    def add_documents(
+        self,
+        documents: Sequence[Document],
+        ids: Optional[Sequence[str]] = None,
+    ) -> None:
+        """Save a list of Documents to Redis.
+
+        Args:
+            documents: A List of Documents.
+            ids: The keys in Redis used to store the Documents. If specified,
+                the length of the IDs must be the same as Documents. If not
+                specified, random UUIDs with prefix are generated to store
+                each Document.
+        """
+        doc_ids = ids
+        if not doc_ids:
+            doc_ids = [self._key_prefix + str(uuid.uuid4()) for _ in documents]
+        if len(documents) != len(doc_ids):
+            raise ValueError("The length of documents must match the length of the IDs")
+
+        for i, doc in enumerate(documents):
+            mapping = self._filter_metadata_by_fields(doc.metadata)
+            mapping.update({self._content_field: json.dumps(doc.page_content)})
+
+            # Remove existing key in Redis to avoid reusing the doc ID.
+            self._redis.delete(doc_ids[i])
+            self._redis.hset(doc_ids[i], mapping=mapping)
+
+    def _filter_metadata_by_fields(self, metadata: Optional[dict]) -> dict:
+        """Filter metadata fields to be stored in Redis.
+
+        Args:
+            metadata: The metadata field of a Document object.
+
+        Returns:
+            dict: A subset dict of the metadata that only contains the fields
+                specified in the initialization of the saver. The value of each
+                key is serialized by JSON.
+        """
+        if not metadata:
+            return {}
+        filtered_fields = (
+            self._metadata_fields & metadata.keys()
+            if self._metadata_fields
+            else metadata.keys()
+        )
+        filtered_metadata = {k: json.dumps(metadata[k]) for k in filtered_fields}
+        return filtered_metadata
---
+++ tests/test_doc_saver.py
@@ -0,0 +1,103 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import json
+import os
+
+import pytest
+import redis
+from langchain_core.documents.base import Document
+
+from langchain_google_memorystore_redis.doc_saver import MemorystoreDocumentSaver
+
+
+@pytest.mark.parametrize(
+    "page_content,metadata,content_field,metadata_fields",
+    [
+        (
+            '"content1"',
+            {"key1": '"doc1_value1"', "key2": '"doc1_value2"'},
+            "page_content",
+            None,
+        ),
+        (
+            '"content2"',
+            {"key1": {'"nested_key"': {'"double_nested"': '"doc2_value1"'}}},
+            "special_page_content",
+            None,
+        ),
+        (
+            '"content3"',
+            {"key1": {"k": "not_in_filter"}, "key2": {'"key"': "in_filter"}},
+            "page_content",
+            set(["key2"]),
+        ),
+    ],
+)
+def test_doc_saver_add_documents_one_doc(
+    page_content, metadata, content_field, metadata_fields
+):
+    client = redis.from_url(get_env_var("REDIS_URL", "URL of the Redis instance"))
+
+    saver = MemorystoreDocumentSaver(
+        client=client,
+        content_field=content_field,
+        metadata_fields=metadata_fields,
+    )
+
+    doc = Document.construct(page_content=page_content, metadata=metadata)
+    doc_id = "doc"
+    saver.add_documents([doc], [doc_id])
+
+    # Only verify the metadata keys given in the metadata_fields
+    metadata_to_verify = {}
+    for k, v in metadata.items():
+        if not metadata_fields or k in metadata_fields:
+            metadata_to_verify[k] = v
+
+    verify_stored_values(
+        client,
+        doc_id,
+        page_content,
+        content_field,
+        metadata_to_verify,
+    )
+
+    client.delete(doc_id)
+
+
+def verify_stored_values(
+    client: redis.Redis,
+    key: str,
+    page_content: str,
+    content_field: str,
+    metadata_to_verify: dict,
+):
+    stored_value = client.hgetall(key)
+    assert isinstance(stored_value, dict)
+    assert len(stored_value) == 1 + len(metadata_to_verify)
+
+    for k, v in stored_value.items():
+        if k == content_field.encode():
+            assert json.loads(v.decode()) == page_content
+        else:
+            assert json.loads(v.decode()) == metadata_to_verify[k.decode()]
+
+
+def get_env_var(key: str, desc: str) -> str:
+    v = os.environ.get(key)
+    if v is None:
+        raise ValueError(f"Must set env var {key} to: {desc}")
+    return v
